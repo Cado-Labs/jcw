180e04537cfef7204539896c58dde4e1d890e265
AS-AlStar <starovojtov.alexander@gmail.com>
2022-05-12 23:28:42 +0300

    add logger

--- lib/jcw/config.rb
+++ lib/jcw/config.rb
@@ -10,6 +10,7 @@ module JCW
                 :tags,
                 :grpc_ignore_methods,
                 :rack_ignore_path_patterns
+                :logger
 
     def enabled
       @enabled ||= false
@@ -43,6 +44,10 @@ module JCW
 
     def rack_ignore_path_patterns
       @rack_ignore_path_patterns ||= []
+    end
+
+    def logger
+      @logger||= ::Logger.new($stdout)
     end
   end
 end
